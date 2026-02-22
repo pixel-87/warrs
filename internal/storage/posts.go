7fa33e3dc6a55b018e34327f378f41a4c2c8361b
pixel <edwardoliverthomas@gmail.com>
2026-02-22 20:49:50 +0000

    feat: add AddPosts

--- internal/storage/posts.go
+++ internal/storage/posts.go
@@ -2,10 +2,37 @@ package storage
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/pixel-87/warss/internal/models"
 )
 
-func (d *DB) SavePosts(feedID int, posts []models.Post) error {
-	return errors.New("not implemented")
+func (d *DB) AddPosts (feedID int, posts []models.Post) error {
+	query := `INSERT INTO posts (
+		feed_id,
+		title,
+		link,
+		content,
+		published_at,
+		updated_at,
+	)
+	VALUES (?, ?, ?, ?, ?, ?)
+	ON CONFLICT(link) DO NOTHING;`
+	
+	for i := range posts {
+		_, err := d.conn.Exec(
+			query,
+			feedID,
+			posts[i].Title,
+			posts[i].Link,
+			posts[i].Content
+			posts[i].PublishedAt
+			posts[i].UpdatedAt
+		)
+
+		if err != nil {
+			return fmt.Errorf("failed to insert post %q for feed %d: %w", posts[i].Title, feedID, err)
+		}
+		return nil
+	}
 }
