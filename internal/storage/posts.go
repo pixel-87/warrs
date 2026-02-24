386b7331835c9ce93816a14164702f2924bb9c4e
pixel <edwardoliverthomas@gmail.com>
2026-02-24 14:18:02 +0000

    feat: add GetPost

--- internal/storage/posts.go
+++ internal/storage/posts.go
@@ -1,7 +1,7 @@
 package storage
 
 import (
-	"errors"
+	"context"
 	"fmt"
 
 	"github.com/pixel-87/warss/internal/models"
@@ -14,7 +14,7 @@ func (d *DB) AddPosts(feedID int, posts []models.Post) error {
 		link,
 		content,
 		published_at,
-		updated_at,
+		updated_at
 	)
 	VALUES (?, ?, ?, ?, ?, ?)
 	ON CONFLICT(link) DO NOTHING;`
@@ -33,6 +33,29 @@ func (d *DB) AddPosts(feedID int, posts []models.Post) error {
 		if err != nil {
 			return fmt.Errorf("failed to insert post %q for feed %d: %w", posts[i].Title, feedID, err)
 		}
-		return nil
 	}
+	return nil
+}
+
+func (d *DB) GetPost(ctx context.Context, postID int) (models.Post, error) {
+	query := `
+		SELECT feed_id, title, link, content, published_at, updated_at
+		FROM posts
+		WHERE id = ?;
+	`
+
+	var p models.Post
+	err := d.conn.QueryRowContext(ctx, query, postID).Scan(
+		&p.FeedID,
+		&p.Title,
+		&p.Link,
+		&p.Content,
+		&p.PublishedAt,
+		&p.UpdatedAt,
+	)
+
+	if err != nil {
+		return models.Post{}, fmt.Errorf("failed to get post id:%d, %w", postID, err)
+	}
+	return p, nil
 }
